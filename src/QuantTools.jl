94d2182126b7875a95e60067312919a40ebb6e15
martingale <124511455+martingale-2910@users.noreply.github.com>
2024-04-14 23:34:42 +0200

    Add dividend, refactor T to tau and improve docs

--- src/QuantTools.jl
+++ src/QuantTools.jl
@@ -4,14 +4,14 @@ using Distributions: Normal, cdf, pdf
 
 Φ(x) = cdf(Normal(), x)
 φ(x) = pdf(Normal(), x)
-d1(S, r, vol, K, T) = (log(S/K) + (r + 0.5*vol^2)*T)/(vol*sqrt(T))
-d2(S, r, vol, K, T) = (log(S/K) + (r - 0.5*vol^2)*T)/(vol*sqrt(T))
+d1(S, q, r, vol, K, tau) = (log(S/K) + ((r - q) + 0.5*vol^2)*tau)/(vol*sqrt(tau))
+d2(S, q, r, vol, K, tau) = (log(S/K) + ((r - q) - 0.5*vol^2)*tau)/(vol*sqrt(tau))
 df(r, t) = exp(-r*t)
 
-@doc raw"""
-    bs_value(S, r, vol, K, T, is_call)
+"""
+    bs_value(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes fair value `V` of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes fair value `V` of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
@@ -21,18 +21,18 @@ julia> bs_value(100., 0.05, 0.2, 110., 1., true)
 6.040088129724232
 ```
 """
-function bs_value(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_value(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call
-        return S*Φ(d1(S, r, vol, K, T)) - K*df(r, T)*Φ(d2(S, r, vol, K, T))
+        return S*df(q, tau)*Φ(d1(S, q, r, vol, K, tau)) - K*df(r, tau)*Φ(d2(S, q, r, vol, K, tau))
     else
-        return K*df(r, T)*Φ(-d2(S, r, vol, K, T)) - S*Φ(-d1(S, r, vol, K, T))
+        return K*df(r, tau)*Φ(-d2(S, q, r, vol, K, tau)) - S*df(q, tau)*Φ(-d1(S, q, r, vol, K, tau))
     end
 end
 
-@doc raw"""
-    bs_delta(S, r, vol, K, T, is_call)
+"""
+    bs_delta(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes delta (```math $\frac{\partial V}{\partial S}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes delta (`` \\frac{\\partial V}{\\partial S}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
@@ -42,153 +42,153 @@ julia> bs_delta(100., 0.05, 0.2, 110., 1., true)
 0.44964793063717595
 ```
 """
-function bs_delta(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_delta(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call
-        return Φ(d1(S, r, vol, K, T))
+        return df(q, tau)*Φ(d1(S, q, r, vol, K, tau))
     else
-        return -Φ(-d1(S, r, vol, K, T))
+        return -df(q, tau)*Φ(-d1(S, q, r, vol, K, tau))
     end
 end
 
-@doc raw"""
-    bs_vega(S, r, vol, K, T, is_call)
+"""
+    bs_vega(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes vega (```math $\frac{\partial V}{\partial \sigma}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes vega (``\\frac{\\partial V}{\\partial \\sigma}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_vega(100., 0.05, 0.2, 110., 1., true)
+julia> bs_vega(100., 0.0, 0.05, 0.2, 110., 1., true)
 39.57604803881934
 ```
 """
-function bs_vega(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_vega(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call  # Both expressions are actually equal
-        return S*sqrt(T)*φ(d1(S, r, vol, K, T))
+        return S*df(q, tau)*sqrt(tau)*φ(d1(S, q, r, vol, K, tau))
     else
-        return K*df(r, T)*φ(d2(S, r, vol, K, T))*sqrt(T)
+        return K*df(r, tau)*sqrt(tau)*φ(d2(S, q, r, vol, K, tau))
     end
 end
 
-@doc raw"""
-    bs_theta(S, r, vol, K, T, is_call)
+"""
+    bs_theta(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes theta (```math $\frac{\partial V}{\partial T}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes theta (``\\frac{\\partial V}{\\partial T}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_theta(100., 0.05, 0.2, 110., 1., true)
+julia> bs_theta(100., 0.0, 0.05, 0.2, 110., 1., true)
 -5.903840050581602
 ```
 """
-function bs_theta(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_theta(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call
-        return -(S*φ(d1(S, r, vol, K, T))*vol)/(2*sqrt(T)) - r*K*df(r, T)*Φ(d2(S, r, vol, K, T))
+        return -df(q, tau)*S*φ(d1(S, q, r, vol, K, tau))*vol/(2*sqrt(tau)) - r*K*df(r, tau)*Φ(d2(S, q, r, vol, K, tau)) + q*S*df(q, tau)*Φ(d1(S, q, r, vol, K, tau))
     else
-        return -(S*φ(d1(S, r, vol, K, T))*vol)/(2*sqrt(T)) + r*K*df(r, T)*Φ(-d2(S, r, vol, K, T))
+        return -df(q, tau)*S*φ(d1(S, q, r, vol, K, tau))*vol/(2*sqrt(tau)) + r*K*df(r, tau)*Φ(d2(S, q, r, vol, K, tau)) - q*S*df(q, tau)*Φ(d1(S, q, r, vol, K, tau))
     end
 end
 
-@doc raw"""
-    bs_rho(S, r, vol, K, T, is_call)
+"""
+    bs_rho(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes rho (```math $\frac{\partial V}{\partial r}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes rho (``\\frac{\\partial V}{\\partial r}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_rho(100., 0.05, 0.2, 110., 1., true)
+julia> bs_rho(100., 0.0, 0.05, 0.2, 110., 1., true)
 38.92470493399336
 ```
 """
-function bs_rho(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_rho(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call
-        return K*T*df(r, T)*Φ(d2(S, r, vol, K, T))
+        return K*T*df(r, tau)*Φ(d2(S, q, r, vol, K, tau))
     else
-        return -K*T*df(r, T)*Φ(-d2(S, r, vol, K, T))
+        return -K*T*df(r, tau)*Φ(-d2(S, q, r, vol, K, tau))
     end
 end
 
-@doc raw"""
-    bs_gamma(S, r, vol, K, T, is_call)
+"""
+    bs_gamma(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes gamma (```math $\frac{\partial^2 V}{\partial \left(S\right)^2}$`) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes gamma (``\\frac{\\partial^2 V}{\\partial \\left(S\\right)^2}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_gamma(100., 0.05, 0.2, 110., 1., true)
+julia> bs_gamma(100., 0.0, 0.05, 0.2, 110., 1., true)
 0.019788024019409666
 ```
 """
-function bs_gamma(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
+function bs_gamma(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
     if is_call  # Both expressions are actually equal
-        return φ(d1(S, r, vol, K, T))/(S*vol*sqrt(T))
+        return df(q, tau)*φ(d1(S, q, r, vol, K, tau))/(S*vol*sqrt(tau))
     else
-        return K*df(r, T)*(φ(d2(S, r, vol, K, T)))/(S*vol*sqrt(T))
+        return df(r, tau)*K*φ(d2(S, q, r, vol, K, tau))/(S^2*vol*sqrt(tau))
     end
 end
 
-@doc raw"""
-    bs_vanna(S, r, vol, K, T, is_call)
+"""
+    bs_vanna(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes vanna (```math $\frac{\partial^2 V}{\partial S \partial \sigma}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes vanna (``\\frac{\\partial^2 V}{\\partial S \\partial \\sigma}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_vanna(100., 0.05, 0.2, 110., 1., true)
+julia> bs_vanna(100., 0.0, 0.05, 0.2, 110., 1., true)
 0.6461797033399724
 ```
 """
-function bs_vanna(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
-    return -φ(d1(S, r, vol, K, T))*d2(S, r, vol, K, T)/vol
+function bs_vanna(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
+    return -df(q, tau)*φ(d1(S, q, r, vol, K, tau))*d2(S, q, r, vol, K, tau)/vol
 end
 
-@doc raw"""
-    bs_volga(S, r, vol, K, T, is_call)
+"""
+    bs_volga(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
 
-Compute the Black-Scholes volga (```math $\frac{\partial^2 V}{\partial \sigma^2}$```) of a European option with Strike `K` expiring in `T` years on an underlying with spot value `S` and volatility `vol` given the interest rate `r`.
+Compute the Black-Scholes volga (``\\frac{\\partial^2 V}{\\partial \\sigma^2}``) of a European option with Strike `K` expiring in `tau` years on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
 # Examples
 ```julia-repl
-julia> bs_volga(100., 0.05, 0.2, 110., 1., true)
+julia> bs_volga(100., 0.0, 0.05, 0.2, 110., 1., true)
 8.17746223872001
 ```
 """
-function bs_volga(S::Float64, r::Float64, vol::Float64, K::Float64, T::Float64, is_call::Bool)
-    return S*sqrt(T)*φ(d1(S, r, vol, K, T))*d1(S, r, vol, K, T)*d2(S, r, vol, K, T)/vol
+function bs_volga(S::Float64, q::Float64, r::Float64, vol::Float64, K::Float64, tau::Float64, is_call::Bool)
+    return df(q, tau)*S*sqrt(tau)*φ(d1(S, q, r, vol, K, tau))*d1(S, q, r, vol, K, tau)*d2(S, q, r, vol, K, tau)/vol
 end
 
-@doc raw"""
-    bs_implied_vol(S, r, V, K, T, is_call)
+"""
+    bs_implied_vol(S::Float64, q::Float64, r::Float64, V::Float64, K::Float64, tau::Float64, is_call::Bool, vol::Float64=0.3, max_iter::Int64=100, tol::Float64=1e-8)
 
-Compute the Black-Scholes implied volatility of a European option with strike `K`, expiring in `T` years and quoted at price `V` on an underlying with spot price `S` and interest rate `r`.
+Compute the Black-Scholes implied volatility of a European option with strike `K`, expiring in `tau` years and quoted at price `V` on an underlying with dividend rate `q`, spot value `S` and volatility `vol` given the interest rate `r`.
 
 The argument `is_call` specifies whether the contract is a Call or a Put.
 
-Uses the Newton-Rhapson method to solve the equation ```math $V = \text{bs_value}(S, r, \sigma_{impl}, K, T, \text{is_call})$``` and converge the the correct value.
+Uses the Newton-Rhapson method to solve the equation ``V = \\text{bs_value}(S, q, r, \\sigma_{impl}, K, tau, \\text{is_call})`` and converge the the correct value.
 
 Assumes an initial starting volatility `vol` of 30% and computes at most `max_iter` (100 by default) iterations with a tolerance of `tol` (set to 1e-8 by default).
 
 # Examples
 ```julia-repl
-julia> bs_implied_vol(100., 0.05, 6.040088129724232, 110., 1., true)
+julia> bs_implied_vol(100., 0.0, 0.05, 6.040088129724232, 110., 1., true)
 0.19999999999999993
 ```
 """
-function bs_implied_vol(S::Float64, r::Float64, V::Float64, K::Float64, T::Float64, is_call::Bool, vol::Float64=0.2, max_iter::Int64=100, tol::Float64=1e-8)
+function bs_implied_vol(S::Float64, q::Float64, r::Float64, V::Float64, K::Float64, tau::Float64, is_call::Bool, vol::Float64=0.3, max_iter::Int64=100, tol::Float64=1e-8)
     for _ in 1:max_iter
-        price = bs_value(S, r, vol, K, T, is_call)
-        vega = bs_vega(S, r, vol, K, T, is_call)
+        price = bs_value(S, q, r, vol, K, tau, is_call)
+        vega = bs_vega(S, q, r, vol, K, tau, is_call)
         diff = V - price
         if abs(diff) < tol
             return vol
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Printf
 using QuantTools: bs_value, bs_delta, bs_theta, bs_vega, bs_gamma, bs_vanna, bs_volga, d1, d2
 
 S = 100.
+q = 0.0
 r = 0.05
 vol = 0.2
 K = 110.
@@ -12,34 +13,34 @@ T = 1.
     # Call option value
     @test begin
         expected_value = 6.040088129724232
-        actual_value = bs_value(S, r, vol, K, T, true)
+        actual_value = bs_value(S, q, r, vol, K, T, true)
         isapprox(actual_value, expected_value; atol=1e-7)
     end
 
     # Call-Put parity
     @test begin
-        call = bs_value(S, r, vol, K, T, true)
-        put = bs_value(S, r, vol, K, T, false)
+        call = bs_value(S, q, r, vol, K, T, true)
+        put = bs_value(S, q, r, vol, K, T, false)
         isapprox(call - put, S - exp(-r*T)*K; atol=1e-7)
     end
 
     # BS Delta, Theta, Gamma
     @test begin
-        call = bs_value(S, r, vol, K, T, true)
-        delta = bs_delta(S, r, vol, K, T, true)
-        theta = bs_theta(S, r, vol, K, T, true)
-        gamma = bs_gamma(S, r, vol, K, T, true)
+        call = bs_value(S, q, r, vol, K, T, true)
+        delta = bs_delta(S, q, r, vol, K, T, true)
+        theta = bs_theta(S, q, r, vol, K, T, true)
+        gamma = bs_gamma(S, q, r, vol, K, T, true)
         isapprox(theta + 0.5*vol^2*S^2*gamma + r*S*delta, r*call; atol=1e-7)
     end
 
     # BS Vega, Vanna, Volga
     @test begin
-        gamma = bs_gamma(S, r, vol, K, T, true)
-        vega = bs_vega(S, r, vol, K, T, true)
+        gamma = bs_gamma(S, q, r, vol, K, T, true)
+        vega = bs_vega(S, q, r, vol, K, T, true)
         isapprox(vega, gamma*S^2*T*vol; atol=1e-7)
-        vanna = bs_vanna(S, r, vol, K, T, true)
-        isapprox(vanna, vega*d2(S, r, vol, K, T)/(S*vol*sqrt(T)); atol=1e-7)
-        volga = bs_volga(S, r, vol, K, T, true)
-        isapprox(volga, vega*d1(S, r, vol, K, T)*d2(S, r, vol, K, T)/(vol); atol=1e-7)
+        vanna = bs_vanna(S, q, r, vol, K, T, true)
+        isapprox(vanna, vega*d2(S, q, r, vol, K, T)/(S*vol*sqrt(T)); atol=1e-7)
+        volga = bs_volga(S, q, r, vol, K, T, true)
+        isapprox(volga, vega*d1(S, q, r, vol, K, T)*d2(S, q, r, vol, K, T)/(vol); atol=1e-7)
     end
 end
